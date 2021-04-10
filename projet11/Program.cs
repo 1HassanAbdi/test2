a1292635dd4e4ab16228e854956c1cec7692827b
1HassanAbdi <hassanabdigaleb@gmail.com>
2021-04-10 10:00:22 -0400

    message1projet11

---
+++ projet11/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projet11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ projet11/projet111.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projet11
+{
+    class projet111
+    {
+        private string nom;
+    }
+}
